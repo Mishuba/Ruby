56510e9dd7b834a4acc9587592e09264ac10cbce
Mishuba <mishuba14@gmail.com>
2021-12-15 15:21:32 -0500

    Two Create Tables

--- EmailBlast/Database/CreateTables.sql
+++ EmailBlast/Database/CreateTables.sql
@@ -33,38 +33,3 @@ create table Press (
     StateOrProvince varchar (75) not null,
     City varchar (100) not null
 );
-
-create table Family (
-    primary key id,
-    FamilyFirstName varchar (50) not null,
-    FamilyLastName varchar (50) not null,
-    FamilyBirthday varchar (50) not null,
-    FamilyWebsite varchar (100) not null,
-    FamilyEmail varchar (100) not null,
-    FamilyPhoneNumber int not null,
-    FamilyAddress varchar (150) not null,
-    FamilyCity varchar (50) not null,
-    FamilyState varchar (100) not null,
-    FamilyCountry varchar (50) not null,
-    FamilyContinent varchar (100) not null,
-    FamilyPlanet varchar (200) not null,
-    FamilyFavoriteColor varchar (50) not null,
-    FamilyFavoriteFood varchar (100) not null,
-    FamilyFavoriteDrink varchar (150) not null,
-    FamilyHobby varchar (100) not null,
-    FamilyTalent varchar (100) not null,
-    FamilyZodiacSign varchar (100) not null,
-    FamilyChineseZodiac varchar (100) not null,
-    FamilyDISC varchar (50) not null,
-    FamilyBusinessPersonalityType varchar (50) not null,
-    FamilyLearningType varchar (50) not null,
-    FamilyLoveLanguage varchar (50) not null,
-    FamilySpiritAnimal varchar (100) not null,
-    FamilyLifeDreamGoal varchar (100) not null,
-    FamilyFavoriteSport varchar (50) not null,
-    FamilyFavoriteSong varchar (100) not null
-);
-
-
-
-
