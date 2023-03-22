46a5dc9709ac462f97ec64625bf9f7c856b5a8e0
Mishuba <mishuba14@gmail.com>
2023-03-22 17:27:29 -0400

    ok

--- EmailBlast/Database/CreateRadioTables.sql
+++ EmailBlast/Database/CreateRadioTables.sql
@@ -1,13 +1,10 @@
-create table Station (
-    primary key id,
-    StationName varchar (100) not null,
-    GeneralEmail varchar (100) not null,
-    MusicSubmit varchar (100) not null,
-    Website varchar (50) not null,
-    Continent varchar (50) not null,
-    Country varchar (75) not null,
-    StateOrProvince varchar (75) not null,
-    City varchar (100) not null
+create table hhet (
+    id int serial primary key,
+    name varchar (75),
+    email varchar (75),
+    website varchar (75),
+    continent varchar (75),
+    country varchar (75)
 );
 
 create table Podcast (
